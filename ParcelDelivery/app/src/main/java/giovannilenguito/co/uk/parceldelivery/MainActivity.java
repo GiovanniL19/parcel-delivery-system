67cd9f5e8d97e41ca90bf8a576c35972d50a2743
Giovanni Lenguito <giovanni16.gl@gmail.com>
2016-10-19 12:53:40 +0100

    Initial Commit

---
+++ ParcelDelivery/app/src/main/java/giovannilenguito/co/uk/parceldelivery/MainActivity.java
@@ -0,0 +1,13 @@
+package giovannilenguito.co.uk.parceldelivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
